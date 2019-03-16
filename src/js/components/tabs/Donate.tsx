f8d56447936b4ed0bc9ba5f96dc69854440ee6a3
Jack Baron <jackmbaron@gmail.com>
2019-03-16 23:08:33 +0000

    Add a SUBTLE hint for the donation prompt

--- src/js/components/tabs/Donate.tsx
+++ src/js/components/tabs/Donate.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import {
   DONATION_LINK,
@@ -10,6 +10,8 @@ import { IState } from '../../store'
 import { setSeenDonationPage } from '../../store/misc'
 import { setCurrentTab } from '../../store/tabs'
 import ExtLink from '../utils/ExtLink'
+
+import '../../../css/donate.css'
 
 interface IProps {
   seenDonationPage: boolean
@@ -25,34 +27,49 @@ const closeMessages = [
   'Maybe some other time.',
 ]
 
-const Donate: FunctionComponent<IProps> = props => (
-  <div className='content'>
-    <h1>Donate</h1>
-    <p>
-      {DONATION_TEXT}
-      <br />
-      <ExtLink href={DONATION_LINK}>{DONATION_LINK_TEXT}</ExtLink>{' '}
-      {DONATION_TEXT_2}
-    </p>
-    <p>
-      <a
-        href='/'
-        onClick={e => {
-          e.preventDefault()
+const Donate: FunctionComponent<IProps> = props => {
+  const [hidden, setHidden] = useState(true)
 
-          if (!props.seenDonationPage) props.setSeenDonationPage(true)
-          props.setCurrentTab(0)
-        }}
-      >
-        <button className='button'>
-          <i>
-            {closeMessages[Math.floor(Math.random() * closeMessages.length)]}
-          </i>
-        </button>
-      </a>
-    </p>
-  </div>
-)
+  useEffect(() => {
+    setTimeout(() => setHidden(false), 10)
+  })
+
+  const hiddenClass = hidden ? ' hidden' : ''
+  return (
+    <>
+      <div className='content'>
+        <h1>Donate</h1>
+        <p>
+          {DONATION_TEXT}
+          <br />
+          <ExtLink href={DONATION_LINK}>{DONATION_LINK_TEXT}</ExtLink>{' '}
+          {DONATION_TEXT_2}
+        </p>
+        <p>
+          <a
+            href='/'
+            onClick={e => {
+              e.preventDefault()
+
+              if (!props.seenDonationPage) props.setSeenDonationPage(true)
+              props.setCurrentTab(0)
+            }}
+          >
+            <button className={`button donate-button${hiddenClass}`}>
+              <i>
+                {
+                  closeMessages[
+                    Math.floor(Math.random() * closeMessages.length)
+                  ]
+                }
+              </i>
+            </button>
+          </a>
+        </p>
+      </div>
+    </>
+  )
+}
 
 const mapStateToProps: (state: IState) => IProps = state => ({
   seenDonationPage: state.misc.seenDonationPage,
